24b878fb0cdd451253168250de6f2946e95140d4
César Luna <cesarlunalimon@gmail.com>
2024-10-24 22:41:06 -0600

    inicio del proyecto

---
+++ Estructura de datos/ProyectoFinal/proyecto_noviembre.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Cards{
+	int id;
+	char titulo;
+	char descripcion;
+	int prioridad;
+	struct Cards* siguiente;
+	struct Cards* anterior;source
+}Card;
+
+struct Cards createCard(){
+	
+}
+
+void menuPrincipal(){
+	int opt;
+	
+	printf("==================================================\n");
+	printf("|                                                |\n");
+	printf("|                                                |\n");
+	printf("|                  To-Do List                    |\n");
+	printf("|                                                |\n");
+	printf("|   Presiona una de las opciones para empezar    |\n");
+	printf("==================================================\n");
+	printf("\n\n");
+	printf("[1] Registrar nueva tarjeta\n");
+	printf("[2] Listar tarjetas\n");
+	printf("[0] Salir\n");
+
+	scanf("%d", &opt);
+	
+	switch(opt){
+		case 0:
+			return;
+		case 1:
+			printf("xd");
+			break;
+		case 2:
+			printf("xd2");
+			break;
+		default:
+			system("cls");
+			menuPrincipal();
+	}
+}
+
+void 
+
+int main(){
+	
+	menuPrincipal();
+	
+	return 0;
+}
