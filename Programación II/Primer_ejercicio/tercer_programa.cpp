186e806ba693941d8aac204852e36600254f6431
César Luna <cesarlunalimon@gmail.com>
2025-01-03 14:37:33 -0600

    minor changes

--- Programación II/Primer_ejercicio/tercer_programa.cpp
+++ Programación II/Primer_ejercicio/tercer_programa.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void session_time(time_t tini){
     time_t tfin = time(NULL);
 
     printf("\n tiempo de inicio: %s", asctime(localtime(&tini)));
-	printf("\n timepo de salida %s", asctime(localtime(&tfin)));
+	printf("\n tiempo de salida %s", asctime(localtime(&tfin)));
 	printf("\n Tiempo de permanencia en el sistema %.2f seg", difftime(tfin, tini));
 }
 
@@ -14,30 +15,38 @@ int main(){
 	time_t tini, tfin;
     tini = time(NULL);
     
-    int lifePoints=3, answer,tryAgain;
+    //configurar la semilla para generar números aleatorios.
+    srand(time(NULL));
     
-    printf("Bienvenido a: adivina el numero\n");
+    //Crea el número con rango máximo de 100
+    int magicNumber=rand()%101;
+    
+    int lifePoints=3, answer, tryAgain;
+    	
+    printf("Número mágico: %d", magicNumber);
     
     do{
-    	if()
-    	int magicNumber=rand()%101;
-    	printf("\n\nIntentos:")
-    	printf("Adivina, adivina...\n");
-    	printf("¿Cual es el numero magico? (Pista: es un entero positivo del 0 al 100 :v)\n");
-    	scanf("%d", &answer);
     	
-    	if(answer==maginNumber){
-    		
-		}else{
-			lifePoints--;
-			
+    	printf(" --------------------------------- \n");
+    	printf("|        Adivina el número.       |\n"):
+    	printf(" --------------------------------- \n");
+    	printf("Presiona 1 para jugar.\n");
+    	printf("Presiona 0 para salir del programa.\n");
+    	scanf("%d", &tryAgain)
+    	
+		if(tryAgain!=0){
+			system("cls");
+			printf("Estoy pensando en un número entero entre el 0 y el 100\n");
+			printf("Adivinas cual es?\n");
+			printf("Respuesta: ");
+			scanf("%d", &answer);	
 		}
-    	
-	}while(lifePoints == 0 && tryAgain == 0);
+		
+		printf("Quieres jugar otra vez? \n");
+		scanf("%d", &tryAgain);
+				
+	}while(tryAgain != 1 );
 	
-	system("cls");
     session_time(tini);
 	return 0;
 }
-
-
