d77519ff3513bb5d070ed60fd67cf891613477b6
César Luna <cesarlunalimon@gmail.com>
2025-07-08 22:52:22 -0600

    [IN PROGRESS] - Less progress than I wish :c

--- programacion_dos/practica_uno/tercer_programa/tercer_programa.cpp
+++ programacion_dos/practica_uno/tercer_programa/tercer_programa.cpp
@@ -10,21 +10,58 @@ void session_time(time_t tini){
   printf("\n Tiempo de permanencia en el sistema %.2f seg", difftime(tfin, tini));
 }
 
+int magicNumberGenerator(){
+  srand(time(NULL));
+  return rand() % (100 + 1);
+}
+
+void playGame(){
+  printf("\033[2J\033[H");
+  int magicNumber = magicNumberGenerator(), answer;
+  bool surrender;
+  
+  do{
+    /*TODO: 
+      - set a counter for intents
+      - set a subquestion for exit if cant doit
+      - for each intent evaluate and told the player is mayor or lesser
+      - if wins, gives another number until player stops game
+     */
+  }while(answer != magicNumber && surrender == false);
+  printf("Magic number is: %d\n", magicNumber);
+}
+
 int main(){
   //Para el contador
-  time_t tini, tfin;
+  time_t tini;
   tini = time(NULL);
-  char answer;
+  int endLoop = 1, answer;
   
   do{
     printf("Juego: Numero magico\n");
     printf("¿Quieres empezar a jugar?\n");
-    printf("[Y]Si/[N]No\n");
-    scanf("%c",&answer);
-    
-    printf("%c", answer);
-    
-  }while(answer!='y');
+    printf("[1]Play/[0]Exit\n");
 
+    scanf("%d", &answer);
+    
+    switch(answer){
+    case 1:
+      //calls game function
+      playGame();
+      break;
+
+    case 0:
+      endLoop = 0;
+      break;
+
+    default:
+      printf("Wrong answer... try again\n");
+      //Limpiamos el buffer de entrada si ingresa un carácter raro
+      while(getchar() != '\n' && getchar() != EOF);
+    }
+   
+  }while(endLoop!=0);
+
+  session_time(tini);
   return 0;
 }
