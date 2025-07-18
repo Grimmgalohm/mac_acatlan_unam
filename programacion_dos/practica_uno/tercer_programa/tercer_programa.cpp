e89f78a4a51ac444a195250fb591eaa4ef4fea12
César Luna <cesarlunalimon@gmail.com>
2025-07-17 22:55:28 -0600

    [DONE] - Tercer programa de la primera practica finalizado.

--- programacion_dos/practica_uno/tercer_programa/tercer_programa.cpp
+++ programacion_dos/practica_uno/tercer_programa/tercer_programa.cpp
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
 void session_time(time_t tini){
   time_t tfin = time(NULL);
   
-  printf("\n tiempo de inicio: %s", asctime(localtime(&tini)));
-  printf("\n tiempo de salida %s", asctime(localtime(&tfin)));
-  printf("\n Tiempo de permanencia en el sistema %.2f seg", difftime(tfin, tini));
+  printf("Tiempo de inicio:\n%s\n", asctime(localtime(&tini)));
+  printf("Tiempo de salida:\n%s\n", asctime(localtime(&tfin)));
+  printf("Tiempo de permanencia en el sistema: \n%.2f seg\n", difftime(tfin, tini));
 }
 
 int magicNumberGenerator(){
@@ -17,18 +17,48 @@ int magicNumberGenerator(){
 
 void playGame(){
   printf("\033[2J\033[H");
-  int magicNumber = magicNumberGenerator(), answer;
+  int magicNumber = magicNumberGenerator(), answer, intents;
   bool surrender;
+  char higher[6]="mayor",lower[6]="menor";
+
+  printf("\t\tJuguemos!\nPara responder ingresa el numero entero que crees es el numero magico\n");
+  printf("Para rendirte y salir, ingresa -1 como respuesta\n");
   
   do{
+    system("clear");
     /*TODO: 
       - set a counter for intents
       - set a subquestion for exit if cant doit
       - for each intent evaluate and told the player is mayor or lesser
       - if wins, gives another number until player stops game
      */
+    
+    printf("Adivina el numero magico...\nEl numero magico es...\n");
+    do{
+      system("clear");
+      scanf("%d", &answer);
+      if(answer<-1) printf("Ingresa un numero entero positivo\n");
+      while(getchar() != '\n' && getchar() != EOF); 
+    }while(answer<-1);
+    
+    intents+=1;
+    
+    if(answer==magicNumber){
+      system("clear");
+      printf("Correcto! Has ganado la partida en %d intentos\n", intents);
+      playGame();
+    }
+    if(answer!=-1){
+      // Condiciones si el número es diferente. Intentemos no hacer if anidados xd
+      system("clear");
+      printf("Whops! Incorrecto... el número es %s\n", answer>magicNumber?lower:higher);
+    }else{
+      system("clear");
+      printf("Tan pronto? Solo has intentado %d veces!\n", intents);
+      printf("El numero magico es: %d", magicNumber);
+      return;
+    }
   }while(answer != magicNumber && surrender == false);
-  printf("Magic number is: %d\n", magicNumber);
 }
 
 int main(){
